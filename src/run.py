bd09ff700469c451d2584552e479a86503c5ed34
Patrick Nanz <patricknanz2002@gmail.com>
2026-03-01 12:13:24 +0100

    feat: implement run_retrieval_pipeline for document retrieval workflow

--- src/run.py
+++ src/run.py
@@ -1,5 +1,6 @@
 from pathlib import Path
-
+from embeddings.embedder import RequirementsEmbedder
+from embeddings.vectorStore import InMemoryVectorStore
 
 ############################################
 ##
@@ -13,6 +14,7 @@ def load_documents_recursive(target_path: Path) -> list[dict]:
     
     documents = []
 
+    # Creating dictionaries as list in documents
     for file in target_path.rglob("*.txt"):
        documents.append({
            "id": file.stem,
@@ -21,19 +23,46 @@ def load_documents_recursive(target_path: Path) -> list[dict]:
 
     return documents
 
+############################################
+##
+## Method: req_text_encoder
+##
+############################################
+def run_retrieval_pipeline(documents):
+    
+    # Creating two lists out of the documents list containing the dictionaries
+    ids = [doc["id"] for doc in documents]
+    texts = [doc["text"] for doc in documents]
+    
+    # Encode all texts getting a list with vectors
+    embedder = RequirementsEmbedder()
+    vectorized_texts = embedder.encode(texts)
+    
+    # Saving vectors 
+    store = InMemoryVectorStore()
+    store.add(ids,vectorized_texts)
 
+    # Creating, encoding and comparing search term
+    query_text = input("Please enter the customer requirement text to compare: ")
+    vectorized_query = embedder.encode([query_text])[0]
+    top_search_results = store.search(vectorized_query, 1)
+    
+    # Creating a dictionary, loading id and text of each entry in documents list
+    doc_lookup = {doc["id"]: doc["text"] for doc in documents}
 
-
-
+    # Getting text for each top_search_result
+    for reqId, score in top_search_results:
+        text = doc_lookup[reqId]
+        print(f'"{text}" — Similarity: {score * 100:.2f}%')
+    
 ############################################
 ##
 ## Run
 ##
 ############################################
-
 data_path = "data/raw"
 base_path = Path(__file__).resolve().parent.parent
 target_path = (base_path / data_path).resolve()
 
 documents = load_documents_recursive(target_path)
-print(documents)
+run_retrieval_pipeline(documents)
